bb0784bd316ccc0d498d9f1e974f41026c6bd676
Gopi Kirupanithi <kgopiaris@gmail.com>
2019-11-18 10:20:04 +0530

    Update altername_sorting.py

--- altername_sorting.py
+++ altername_sorting.py
@@ -1,15 +1,16 @@
 
 def rearrange(a):
-	b= []
-	c = len(a)-1
-	for i in range(len(a)):
-		b.append(a[c])
-		if(c != i):
-			b.append(a[i])
-			c = c-1
-		else:
-			break
-	return b
+ b= []
+ y = c = len(a)-1
+ for i in range(len(a)):
+  if len(b) < (y+1):
+   b.append(a[c])
+  if(i < c):
+   b.append(a[i])
+   c = c-1
+  else:
+   break
+ return b
 
 list_a = [1,2,3,4,5,6,7]
 print "Input value is: ", list_a
